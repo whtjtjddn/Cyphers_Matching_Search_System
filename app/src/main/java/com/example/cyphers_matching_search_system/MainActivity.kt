7180c928383406613218687a347027174f1a1876
whtjtjddn <54929329+whtjtjddn@users.noreply.github.com>
2021-08-31 16:47:05 +0900

    convert java to kotlin

--- app/src/main/java/com/example/cyphers_matching_search_system/MainActivity.kt
+++ app/src/main/java/com/example/cyphers_matching_search_system/MainActivity.kt
@@ -1,86 +1,62 @@
-package com.example.cyphers_matching_search_system;
+package com.example.cyphers_matching_search_system
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONArray
+import org.json.JSONObject
+import android.os.Bundle
+import com.example.cyphers_matching_search_system.R
+import android.content.Intent
+import android.view.View
+import android.widget.Button
+import com.example.cyphers_matching_search_system.MainActivity2
+import android.widget.EditText
+import android.widget.TextView
+import com.example.cyphers_matching_search_system.MainActivity
+import org.json.JSONException
+import java.util.concurrent.ExecutionException
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.concurrent.ExecutionException;
-
-public class MainActivity extends AppCompatActivity {
-
-    static String taskUrl;
-    private String Nickname;
-    private JSONArray jsonId;
-    private String Id;
-    private JSONObject jsonObject1;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-
-        EditText editText = (EditText) findViewById(R.id.nickname);
-        Button button = (Button) findViewById(R.id.searchMatchHistory_btn);
-        TextView textView = (TextView) findViewById(R.id.matchHistory);
-
-        final String[] resultText = {null};
-        final String[] resultText1 = {null};
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Nickname = editText.getText().toString();
-                try {
-                    taskUrl = String.format("https://api.neople.co.kr/cy/players?nickname=%s&wordType=full&apikey=FnaA38BJKLS69mQ9rDx6vEztIr8fbS3y", Nickname);
-                    resultText[0] = new Task().execute().get();
-
-                    JSONObject jsonObject = new JSONObject(resultText[0]);
-                    jsonId = jsonObject.getJSONArray("rows");
-
-                    for(int i = 0;i<jsonId.length();i++)
-                    {
-                        jsonObject1 = jsonId.getJSONObject(i);
-                    }
-                    Id = jsonObject1.getString("playerId");
-
-                    taskUrl = String.format("https://api.neople.co.kr/cy/players/%s/matches?gameTypeId=<gameTypeId>&startDate=<startDate>&endDate=<endDate>&limit=<limit>&next=<next>&apikey=FnaA38BJKLS69mQ9rDx6vEztIr8fbS3y",Id);
-
-                    resultText1[0] = new Task().execute().get();
-
+class MainActivity : AppCompatActivity() {
+    private var Nickname: String? = null
+    private var jsonId: JSONArray? = null
+    private var Id: String? = null
+    private var jsonObject1: JSONObject? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val intent = Intent(this@MainActivity, MainActivity2::class.java)
+        val editText = findViewById<View>(R.id.nickname) as EditText
+        val button = findViewById<View>(R.id.searchMatchHistory_btn) as Button
+        val textView = findViewById<View>(R.id.matchHistory) as TextView
+        val resultText = arrayOf<String?>(null)
+        val resultText1 = arrayOf<String?>(null)
+        button.setOnClickListener {
+            Nickname = editText.text.toString()
+            try {
+                taskUrl = String.format("https://api.neople.co.kr/cy/players?nickname=%s&wordType=full&apikey=FnaA38BJKLS69mQ9rDx6vEztIr8fbS3y", Nickname)
+                resultText[0] = Task().execute().get()
+                val jsonObject = JSONObject(resultText[0])
+                val jsonId = jsonObject.getJSONArray("rows")
+                for (i in 0 until jsonId.length()) {
+                    jsonObject1 = jsonId.getJSONObject(i)
                 }
-                catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                catch (ExecutionException e) {
-                    e.printStackTrace();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                textView.setText(resultText1[0]);
-                intent.putExtra("match_Json", resultText1[0]);
-                startActivity(intent);
+                Id = jsonObject1!!.getString("playerId")
+                taskUrl = String.format("https://api.neople.co.kr/cy/players/%s/matches?gameTypeId=<gameTypeId>&startDate=<startDate>&endDate=<endDate>&limit=<limit>&next=<next>&apikey=FnaA38BJKLS69mQ9rDx6vEztIr8fbS3y", Id)
+                resultText1[0] = Task().execute().get()
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            } catch (e: ExecutionException) {
+                e.printStackTrace()
+            } catch (e: JSONException) {
+                e.printStackTrace()
             }
-        });
+            textView.text = resultText1[0]
+            intent.putExtra("match_Json", resultText1[0])
+            startActivity(intent)
+        }
+    }
 
+    companion object {
+        @JvmField
+        var taskUrl: String? = null
     }
 }
-/*
-void JSONParse(String jsonString) throws JSONException {
-    String myId;
-    JSONArray jsonArray = new JSONArray(jsonString);
-    for(int i = 0; i<jsonArray.length();i++)
-    {
-        JSONObject jsonObject = jsonArray.getJSONObject(i);
-    }
-}*/
