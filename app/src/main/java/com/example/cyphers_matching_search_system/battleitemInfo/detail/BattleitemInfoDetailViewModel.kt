24e5f2c9d031995e83429712c8f60fdde43e8a9c
여영호 <yyh3829@naver.com>
2021-10-01 21:20:30 +0900

    set text color

--- app/src/main/java/com/example/cyphers_matching_search_system/battleitemInfo/detail/BattleitemInfoDetailViewModel.kt
+++ app/src/main/java/com/example/cyphers_matching_search_system/battleitemInfo/detail/BattleitemInfoDetailViewModel.kt
@@ -1,7 +1,9 @@
 package com.example.cyphers_matching_search_system.battleitemInfo.detail
 
+import android.annotation.SuppressLint
 import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
+import com.example.cyphers_matching_search_system.R
 
 class BattleitemInfoDetailViewModel: ViewModel() {
 
@@ -10,6 +12,7 @@ class BattleitemInfoDetailViewModel: ViewModel() {
     var _battleitemName = ObservableField<String>()
     var _battleitemExplain = ObservableField<String>()
     var _battleitemExplainDetail = ObservableField<String>()
+    var _battleitemNameColor = ObservableField(R.color.black)
 
     fun loadData(battleitemId: String){
         val defaultUrl = "https://img-api.neople.co.kr/cy/items/"
@@ -17,13 +20,27 @@ class BattleitemInfoDetailViewModel: ViewModel() {
             {
                 _battleitemImage.set(defaultUrl+it.itemId)
                 _battleitemName.set(it.itemName)
-                _battleitemExplain.set(it.explain)
+                _battleitemNameColor.set(choiceColor(it.rarityCode))
+                _battleitemExplain.set("공백")
                 _battleitemExplainDetail.set(it.explainDetail)
             },
             {_, t ->
                 run { _battleitemExplainDetail.set("error:" + t.message) }
             }
         )
+    }
+
+    @SuppressLint("ResourceAsColor")
+    fun choiceColor(rarity: String): Int {
+        var color = R.color.black
+        when (rarity) {
+            "101" -> color = R.color.common
+            "102" -> color = R.color.uncommon
+            "103" -> color = R.color.rare
+            "104" -> color = R.color.unique
+        }
+
+        return color
     }
 
 }
